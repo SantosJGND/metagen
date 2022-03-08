5cd156183e5c98dda34937e96d3fb605f08e7400
SantosJGND <joao.dourado@insa.saude-min.pt>
2022-03-08 14:31:36 +0000

    removed hardcoded django secret key

--- metagen/settings.py
+++ metagen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mwq4_v77$fqy=qw%%a7wytjszi8z-!=g8#1o)u=xjis!d4m#gh'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
